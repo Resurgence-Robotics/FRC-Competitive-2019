bfdc0b25db3f0fdfbf880de836a8c5118540a75f
Unknown_Laptop\ERIC <etstb3@gmail.com>
2019-01-17 19:28:07 -0500

    2 Versions of Mecanum code,  both need testing

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -12,6 +12,9 @@ Robot::Robot() {
 }
 
 const double THRESHOLD = 0.1;
+const double WHEEL_RADIUS = 3;
+const double WHEEL_SEPERATION_WIDTH = 23/2;
+const double WHEEL_SEPERATION_LENGTH = 27/2;
 
 frc::Joystick stick0{0};
 frc::Joystick stick1{1};
@@ -20,6 +23,11 @@ TalonSRX driveLF = {12};
 TalonSRX driveLR = {13};
 TalonSRX driveRF = {4};
 TalonSRX driveRR = {6};
+
+double LF_Out;
+double LR_Out;
+double RF_Out;
+double RR_Out;
 
 double fabs(double input){
 	if(input >= 0){
@@ -55,27 +63,31 @@ void Robot::Autonomous() {
 
 void Robot::OperatorControl() {
 	while (IsOperatorControl() && IsEnabled()) {
-		double leftStick = -stick0.GetY();
-		double rightStick = stick1.GetY();
+		//Lazy mecanum
+		double Y_in = (fabs(stick0.GetY()) > THRESHOLD) ? stick0.GetY() : 0;
+		double X_in = (fabs(stick0.GetX()) > THRESHOLD) ? stick0.GetX() : 0;
+		double Z_in = (fabs(stick0.GetZ()) > THRESHOLD) ? stick0.GetZ() : 0;
 
-		if(fabs(leftStick) > THRESHOLD){
-			driveLF.Set(ControlMode::PercentOutput, leftStick);
-			driveLR.Set(ControlMode::PercentOutput, leftStick);
-		}
-		else{
-			driveLR.Set(ControlMode::PercentOutput, 0);
-			driveLF.Set(ControlMode::PercentOutput, 0);
-		}
+		LF_Out = (1/WHEEL_RADIUS) * (X_in - Y_in - (WHEEL_SEPERATION_LENGTH + WHEEL_SEPERATION_WIDTH) * Z_in);
+		LR_Out = (1/WHEEL_RADIUS) * (X_in + Y_in - (WHEEL_SEPERATION_LENGTH + WHEEL_SEPERATION_WIDTH) * Z_in);
+		RF_Out = (1/WHEEL_RADIUS) * (X_in + Y_in + (WHEEL_SEPERATION_LENGTH + WHEEL_SEPERATION_WIDTH) * Z_in);
+		RR_Out = (1/WHEEL_RADIUS) * (X_in - Y_in + (WHEEL_SEPERATION_LENGTH + WHEEL_SEPERATION_WIDTH) * Z_in);
 		
-		if(fabs(rightStick) > THRESHOLD){
-			driveRF.Set(ControlMode::PercentOutput, rightStick);
-			driveRR.Set(ControlMode::PercentOutput, rightStick);
-		}
-		else{
-			driveRR.Set(ControlMode::PercentOutput, 0);
-			driveRF.Set(ControlMode::PercentOutput, 0);
-		}
+		//Fancy Mecanum
+		double Magnitude = sqrt((stick0.GetY() * stick0.GetY()) + (stick0.GetX() * stick0.GetX()));
+		double Angle = atan(stick0.GetY()/stick0.GetX());
+		double Twist = stick0.GetZ();
+
+		RR_Out = Magnitude * sin(Angle + (3.14159/4)) - Twist;
+		LF_Out = Magnitude * sin(Angle + (3.14159/4)) + Twist;
+		LR_Out = Magnitude * cos(Angle + (3.14159/4)) + Twist;
+		RF_Out = Magnitude * cos(Angle + (3.14159/4)) - Twist;
 		
+		driveLF.Set(ControlMode::PercentOutput, LF_Out);
+		driveLR.Set(ControlMode::PercentOutput, LR_Out);
+		driveRF.Set(ControlMode::PercentOutput, RF_Out);
+		driveRR.Set(ControlMode::PercentOutput, RR_Out);
+
 		frc::Wait(0.005);// The motors will be updated every 5ms
 	}
 }
